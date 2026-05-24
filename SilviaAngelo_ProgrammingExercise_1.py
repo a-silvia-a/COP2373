1f8b81fb0036c31b39a856d1cacfbdad86b6a696
a-silvia-a <arisbeth345@gmail.com>
2026-05-24 16:17:58 -0400

    commit

--- SilviaAngelo_ProgrammingExercise_1.py
+++ SilviaAngelo_ProgrammingExercise_1.py
@@ -1,4 +1,7 @@
-#Program will abc
-print("hello world")
+#program will pre-sell a limited number of cinema tickets.
+#Each buyer can buy up to 4 tickets.
+#No more than 20 tickets can be sold total.
+# Prompt the user for the desired number of tickets and then display the number of remaining tickets
+#Then display the total number of buyers.
 
-print("test")
+print ("Hello World")
